909e549f8b06f664f41744576c6faba16b6501c1
Yuki Ao <aoyingxue@gmail.com>
2020-12-03 10:17:41 -0600

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# webdev-todo-js-app
-Web development: To-Do list app using javascript and AJAX (API key needed)
+You will create a ToDo app where users can add todos, check off todos as completed, and delete todos. You must use the provided API to list, save, modify, and delete your todos via AJAX in Javascript. 
+
+## Assignment Objectives
+
+- UI Requirements
+    - Input area for adding new ToDos
+        - May also need "add" button
+    - List of ToDos
+    - Each ToDo has:
+        - Checkbox (or another way of checking off) to complete
+        - Name of ToDo
+        - Button to delete
+    - Style Requirements
+        - App should be nicely styled, no elements should look like default HTML elements
+        - App should be centered on the page, and liquid layout so it works well on mobile
+            - Media queries not required but may be useful
+        - New ToDo box should not look like a default input
+        - ToDos should be separated from each other somehow
+        - ToDo controls (check off, delete) should be nicely styled
+- Functionality Requirements
+    - Adding a new ToDo can be done both by pressing enter and when clicking a button. This is best accomplished by executing your code when the form is submitted, instead of when the user presses a button.
+        - After adding a ToDo, the input area for new ToDos should become blank
+        - New ToDos added to list
+    - Checking off a ToDo should cause it to stay on the list but be styled differently (maybe strikethrough, or faded, etc)
+    - Deleting a ToDo should remove it from the list entirely
+- Data Requirements
+    - When page loads, existing ToDos are loaded from the server
+    - When new ToDo is added, send it to the server
+    - When a ToDo is "checked off" or deleted, send that to the server
+## API Key
+    To interact with the API, you will first need an API Key that will uniquely identify your user. This API key cannot be recovered if you lose it, but you can always generate a new one and you will start with a "clean slate" with no data. Generate an API Key here: http://cse204.work/api-key/
+## API Documentation
+    The API has been thoroughly documented, along with some examples for interacting with it via javascript. You can view this documentation here: http://cse204.work/todo-api/ (Links to an external site.)
+
+## Javascript Documentation
+
+This assignment will require heavy use of Javascript to interact with the API, write the data to the page, and provide the user interaction. While it is recommended to use plain Javascript, you may also use jQuery, Axios, or another Javascript framework, although keep in mind these frameworks will not be covered as heavily in class and you will be on your own to get them working correctly.
+
+- Javascript DOM Manipulation Resources:
+    https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Introduction (Links to an external site.) 
+    https://www.sitepoint.com/dom-manipulation-vanilla-javascript-no-jquery/ (Links to an external site.)
+    https://blog.garstasio.com/you-dont-need-jquery/dom-manipulation/ (Links to an external site.)
+- Javascript AJAX Resources:
+    https://developer.mozilla.org/en-US/docs/Web/Guide/AJAX/Getting_Started (Links to an external site.)
+    https://www.w3schools.com/xml/ajax_intro.asp (Links to an external site.)
+    Parsing JSON: https://www.w3schools.com/js/js_json_intro.asp (Links to an external site.)
+- Alternative tools:
+    https://jquery.com (Links to an external site.) (note: if using Bootstrap, by default it only includes a "slim" version fo jquery that does not include $.ajax. Make sure you replace the Bootstrap jquery with the full version.
+    https://github.com/axios/axios (Links to an external site.) - AJAX alternative
+    https://davidwalsh.name/fetch  (Links to an external site.)- AJAX alternative
+    https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch (Links to an external site.) - Good Fetch tutorial
+## Extra Credit
+- Checkboxes act as a "toggle", where clicking the checkbox will invert the todo item's "complete" status.
+- Provide a way for users to update and save the text of a todo item using the API.
---
+++ script.js
@@ -0,0 +1,228 @@
+// todo list element
+const todolist = document.getElementById('todo-list');
+
+// api key
+const apiKey = '9a7a46-c55be6-55eb1f-861696-a468db';
+const apiUrl = 'https://cse204.work/todos';
+
+
+// UI add to-do list
+function addNewToDo(toDoId, toDoItem, toDoComplete) {
+    var listItem = '';
+
+    if (toDoComplete == false) {
+        // create new to-do item
+        listItem = `
+        <li class="d-flex justify-content-between align-items-center">
+            <p class="text">${toDoItem}</p>
+            <span>
+                <i class="far fa-circle todo-icon todo-icon-complete" id="circle-${toDoId}"></i>
+                <i class="far fa-trash-alt todo-icon todo-icon-delete" id="trash-${toDoId}"></i>
+            </span>
+        </li>
+        `;
+        // paste the html element 
+        todolist.insertAdjacentHTML('beforeend', listItem);
+    } else {
+        // create new to-do item
+        listItem = `
+        <li class="d-flex justify-content-between align-items-center">
+            <p class="text checked">${toDoItem}</p>
+            <span>
+                <i class="far fa-check-circle todo-icon todo-icon-complete" id="circle-${toDoId}"></i>
+                <i class="far fa-trash-alt todo-icon todo-icon-delete" id="trash-${toDoId}"></i>
+            </span>
+        </li>
+        `;
+        // paste the html element 
+        todolist.insertAdjacentHTML('beforeend', listItem);
+    }
+
+
+}
+
+// UI
+function completeToDoItem(element) {
+    // icons for check and uncheck
+    const check = 'fa-check-circle';
+    const uncheck = 'fa-circle';
+
+    // toggle classes between check and uncheck
+    element.classList.toggle(check);
+    element.classList.toggle(uncheck);
+    element.parentNode.parentNode.querySelector('.text').classList.toggle('checked');
+
+}
+
+// UI
+function deleteToDoItem(element) {
+    element.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode);
+}
+
+// PUT ToDo Updates
+function putToDoUpdates(e) {
+    e.preventDefault();
+
+    // find the clicked target item
+    const element = e.target;
+
+    // get the updated element id
+    const elementId = element.id.substring(7,); // id after "circle-"
+
+    // get the updated element completed status
+    const elementComplete = element.completed;
+    if (elementComplete == true) {
+        // data
+        var data = {
+            completed: false
+        }
+
+    } else {
+        var data = {
+            completed: true
+        }
+    }
+    const elementTypes = element.classList;
+
+    var xhttp3 = new XMLHttpRequest();
+    xhttp3.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            completeToDoItem(element);
+
+        } else if (this.readyState == 4) {
+            // this.status !== 200, error from server
+            console.log(this.responseText);
+
+        }
+    }
+
+    let url = apiUrl.concat(`/${elementId}`);
+    xhttp3.open("PUT", url, true);
+
+    xhttp3.setRequestHeader("Content-type", "application/json");
+    xhttp3.setRequestHeader("x-api-key", apiKey);
+    xhttp3.send(JSON.stringify(data));
+    console.log('update successfully!');
+}
+
+// DELETE a ToDo
+function deleteToDoItem(e) {
+    e.preventDefault();
+
+    // find the clicked target item
+    const element = e.target;
+
+    // get the updated element id
+    const elementId = element.id.substring(6,); // id after "trash-"
+
+    var xhttp4 = new XMLHttpRequest();
+
+    xhttp4.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            element.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode);
+
+        } else if (this.readyState == 4) {
+            console.log(this.responseText);
+        }
+    }
+
+
+    let url = apiUrl.concat(`/${elementId}`);
+    xhttp4.open("DELETE", url, true);
+    xhttp4.setRequestHeader("x-api-key", apiKey);
+    xhttp4.send();
+    console.log('delete successfully!');
+}
+
+
+// LIST existing todos when refreshing
+function listAllToDos() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var todos = JSON.parse(this.responseText);
+
+            for (i = 0; i < todos.length; i++) {
+                let id = todos[i].id;
+                let toDoItem = todos[i].text;
+                let toDoComplete = todos[i].completed;
+
+                addNewToDo(id, toDoItem, toDoComplete);
+            }
+        }
+    }
+    xhttp.open('GET', apiUrl, true);
+    xhttp.setRequestHeader("x-api-key", apiKey);
+    xhttp.send();
+}
+
+
+// POST a new todo item
+function postNewToDo(e) {
+    e.preventDefault(); // This prevents the form from reloading the page!
+
+    // Setting variable for form input (get from HTML form)
+    let toDoItem = document.getElementById('input').value;
+
+    // if there's any characters in the input
+    if (toDoItem) {
+        var data = {
+            text: toDoItem
+        }
+
+        // Initalize AJAX Request
+        var xhttp2 = new XMLHttpRequest();
+
+        // Response handler
+        xhttp2.onreadystatechange = function () {
+
+            // Wait for readyState = 4 & 200 response
+            if (this.readyState == 4 && this.status == 200) {
+
+                // parse JSON response
+                var todo = JSON.parse(this.responseText);
+
+                // put todo on 
+                addNewToDo(todo.id, toDoItem,false);
+
+                // clear the input
+                document.getElementById('input').value = '';
+
+            } else if (this.readyState == 4) {
+                // this.status !== 200, error from server
+                console.log(this.responseText);
+
+            }
+        };
+
+        xhttp2.open("POST", apiUrl, true);
+
+        xhttp2.setRequestHeader("Content-type", "application/json");
+        xhttp2.setRequestHeader("x-api-key", apiKey);
+        xhttp2.send(JSON.stringify(data));
+        console.log('sent successfully!');
+    }
+}
+function completeOrDelete(e) {
+    console.log(e.target);
+    if (e.target.classList.contains('todo-icon-delete')) {
+        console.log('delete');
+        deleteToDoItem(e);
+    } else if (e.target.classList.contains('todo-icon-complete')) {
+        console.log('complete');
+        putToDoUpdates(e);
+    }
+}
+
+// load all to-do items
+listAllToDos();
+
+// if press enter, call addNewToDo
+const formElement = document.getElementById("todo-form");
+formElement.addEventListener("submit", e => postNewToDo(e), false);
+
+// if clicked, complete to-do item or delete
+document.addEventListener('click', e => completeOrDelete(e));
+
+// Extra Credit: Provide a way for users to update and save the text of a todo item using the API.
